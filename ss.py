a24bc87936a2caa71b16c5a07f480040e8c0fbdb
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-09-01 18:49:19 +0300

    Update ss.py

--- ss.py
+++ ss.py
@@ -2,9 +2,10 @@ import numpy as np
 import pickle
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
+
 from sklearn import svm
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 # loading the diabetes dataset to a pandas DataFrame
 diabetes_dataset = pd.read_csv('diabetes.csv')
