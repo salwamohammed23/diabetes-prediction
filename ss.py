cb68b6a12b5c5cc13f826ea88202b814788ca2f4
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-09-01 17:58:25 +0300

    dataset

--- ss.py
+++ ss.py
@@ -7,29 +7,31 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score
 
 # loading the diabetes dataset to a pandas DataFrame
-diabetes_dataset = pd.read_csv('Downloads/diabetes.csv')
+diabetes_dataset = pd.read_csv('C:\Users\سلوىمحمدعبداللطيفمحم\Documents\GitHub\diabetes_prediction/diabetes.csv')
 # printing the first 5 rows of the dataset
 diabetes_dataset.head()
+
 # separating the data and labels
-X = diabetes_dataset.drop(columns = 'Outcome', axis=1)
+X = diabetes_dataset.drop(columns='Outcome', axis=1)
 Y = diabetes_dataset['Outcome']
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, stratify=Y, random_state=2)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
+
 classifier = svm.SVC(kernel='linear')
-#training the support vector Machine Classifier
+# training the support vector Machine Classifier
 classifier.fit(X_train, Y_train)
+
 # accuracy score on the test data
 X_test_prediction = classifier.predict(X_test)
 test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
-print('Accuracy score of the test data : ', test_data_accuracy)
+
+# Saving the trained model
 filename = 'trained_model.sav'
 pickle.dump(classifier, open(filename, 'wb'))
-# loading the saved model
+
+# Loading the saved model
 loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
-# loading the saved model
-#loaded_model = pickle.load(open('E:/streamlit/trained_model.sav', 'rb'))
-
-# creating a function for prediction
+# Creating a function for prediction
 def diabetes_prediction(input_data):
     # changing the input_data to numpy array
     input_data_as_numpy_array = np.asarray(input_data)
@@ -45,9 +47,9 @@ def diabetes_prediction(input_data):
         return 'The person is diabetic'
 
 def main():
-    # giving title
+    # Giving title
     st.title('Diabetes Prediction App')
-    # getting the input data
+    # Getting the input data
     Pregnancies = st.text_input('No. of Pregnancies')
     Glucose = st.text_input('No. of Glucose')
     BloodPressure = st.text_input('No. of Blood Pressure')
@@ -55,10 +57,9 @@ def main():
     Insulin = st.text_input('No. of Insulin')
     BMI = st.text_input('No. of BMI')
     DiabetesPedigreeFunction = st.text_input('No. of Diabetes Pedigree Function')
-   
     Age = st.text_input('No. of Age')
 
-    # code for prediction
+    # Code for prediction
     diagnosis = ""
     if st.button('Diabetes test result'):
         diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI,
